af9bffa4586485798652d5945b52ef456818b5b8
wladziaya <66369788+wladziaya@users.noreply.github.com>
2021-10-28 17:12:52 +0000

    add a multiplication operation

--- calculator.cpp
+++ calculator.cpp
@@ -5,7 +5,22 @@ int Calculator::Add (int a, int b)
     return a + b;
 }
 
+int Calculator::Sub (int a, int b)
+{
+    return Add (a, -b);
+}
+
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
+}
+
+int Calculator::Del (double a, double b)
+{
+    return a / b + 0.5;
+}
+
+int Calculator::Mod (int a, int b)
+{
+    return a % b;
 }
